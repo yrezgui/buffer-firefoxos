fd6cb98dc42c326b131ff7455e55f167a3eece72
Yacine Rezgui <rezgui.y@gmail.com>
2014-07-24 20:22:00 +0100

    Add Profile page

---
+++ src/app/profile/profileCtrl.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  var mod = angular.module('bufferApp.profile', []);
+
+  mod.controller('ProfileCtrl', ['$scope', '$stateParams', 'OAuth', 'BufferApi', 'profiles', 'currentProfile', function ProfileCtrl($scope, $stateParams, OAuth, BufferApi, profiles, currentProfile) {
+
+    $scope.showSidebar        = false;
+    $scope.showSentTab        = false;
+    $scope.loadingData        = true;
+    $scope.actionsMenuHidden  = true;
+    $scope.selectedStatus     = null;
+    $scope.profiles           = profiles;
+    $scope.currentProfile     = currentProfile;
+
+    function getUpdates(type, forceRefresh) {
+      if(type === 'sent') {
+
+        BufferApi.getSentUpdates($scope.currentProfile._id, forceRefresh).then(function success(result) {
+          $scope.sentUpdates = result.updates;
+          $scope.loadingData = false;
+        });
+      } else {
+
+        BufferApi.getPendingUpdates($scope.currentProfile._id, forceRefresh).then(function success(result) {
+          $scope.pendingUpdates = result.updates;
+          $scope.loadingData = false;
+        });
+      }
+    }
+
+    $scope.showTab = function showTab(name, forceRefresh) {
+      $scope.loadingData = true;
+
+      if(name === 'sent') {
+        $scope.showSentTab = true;
+        getUpdates('sent', forceRefresh);
+
+      } else {
+        $scope.showSentTab = false;
+        getUpdates('pending', forceRefresh);
+      }
+    };
+
+    $scope.showActionsMenu = function showActionsMenu(status) {
+      $scope.selectedStatus    = status;
+      $scope.actionsMenuHidden = false;
+    };
+
+    $scope.hideActionsMenu = function hideActionsMenu() {
+      $scope.selectedStatus    = null;
+      $scope.actionsMenuHidden = true;
+    };
+
+    $scope.shareNowStatus = function shareNowStatus() {
+      return BufferApi.shareNow($scope.selectedStatus._id).then(function success() {
+        $scope.showTab('pending', true);
+        $scope.hideActionsMenu();
+      });
+    };
+
+    $scope.deleteStatus = function deleteStatus() {
+      return BufferApi.deleteStatus($scope.selectedStatus._id).then(function success() {
+        $scope.showTab('pending', true);
+        $scope.hideActionsMenu();
+      });
+    };
+
+    // Default behavior of the page
+    $scope.showTab('pending');
+  }]);
+
+}());
