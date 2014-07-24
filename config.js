0514ed6246529aa407e1aea44b3e5abfcffd0b2d
Yacine Rezgui <rezgui.y@gmail.com>
2014-07-24 19:46:24 +0100

    Update list of assets

--- config.js
+++ config.js
@@ -18,22 +18,23 @@ config.vendor = {
     'bower_components/jquery/dist/jquery.js',
     'bower_components/lodash/dist/lodash.js',
     'bower_components/angular/angular.js',
-    'bower_components/pouchdb/dist/pouchdb-nightly.js',
     'bower_components/angular-animate/angular-animate.js',
     'bower_components/angular-sanitize/angular-sanitize.js',
     'bower_components/angular-touch/angular-touch.js',
     'bower_components/angular-ui-router/release/angular-ui-router.js',
-    'bower_components/restangular/dist/restangular.js'
+    'bower_components/angular-cache/dist/angular-cache.js'
   ],
   styles: [
     'bower_components/building-blocks/util.css',
     'bower_components/building-blocks/fonts.css',
     'bower_components/building-blocks/transitions.css',
     'bower_components/building-blocks/icons/**/*.css',
-    'bower_components/building-blocks/style/**/*.css'
+    'bower_components/building-blocks/style/**/*.css',
+    'bower_components/building-blocks/style_unstable/**/*.css'
   ],
   images: [
     'bower_components/building-blocks/icons/**/*.png',
-    'bower_components/building-blocks/style/**/*.png'
+    'bower_components/building-blocks/style/**/*.png',
+    'bower_components/building-blocks/style_unstable/**/*.png'
   ]
 };
