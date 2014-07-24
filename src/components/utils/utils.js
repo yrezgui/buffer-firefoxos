7df03e097e14f89e19a307fcf69f7ddae1460db1
Yacine Rezgui <rezgui.y@gmail.com>
2014-07-24 19:44:44 +0100

    Add Utils component

---
+++ src/components/utils/utils.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  var mod = angular.module('components.utils', []);
+
+  mod.factory('Utils', ['$window', function Utils($window) {
+
+    function stringify(obj, prefix) {
+      var str = [];
+
+      for(var p in obj) {
+        var k = prefix ? prefix + '[' + p + ']' : p, v = obj[p];
+        str.push(typeof v == 'object' ?
+          stringify(v, k) :
+          encodeURIComponent(k) + '=' + encodeURIComponent(v));
+      }
+
+      return str.join('&');
+    }
+
+    function objectToFormData(data) {
+      var formData  = new $window.FormData();
+
+      for(var i in data) {
+        if(_.isObject(data[i])) {
+
+          for(var j in data[i]) {
+            formData.append(i + '[' + j + ']', data[i][j]);
+          }
+
+        } else if(_.isArray(data[i])) {
+
+          for(var k in data[i]) {
+            formData.append(i + '[]', data[i][k]);
+          }
+
+        } else {
+          formData.append(i, data[i]);
+        }
+      }
+
+      return formData;
+    }
+
+    return {
+      stringify: stringify,
+      objectToFormData: objectToFormData
+    };
+  }]);
+
+}());
