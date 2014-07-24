d5c8e9705194ace528b09353373c04b6068b4e53
Yacine Rezgui <rezgui.y@gmail.com>
2014-07-24 20:22:14 +0100

    Add app states

--- src/app/bufferApp.js
+++ src/app/bufferApp.js
@@ -15,9 +15,48 @@ angular.module('bufferApp', [
   'bufferApp.profile'
 ])
 
-  .config(['$compileProvider', '$urlRouterProvider', '$stateProvider', function config($compileProvider, $urlRouterProvider, $stateProvider) {
+  .config(['$urlRouterProvider', '$stateProvider', function config($urlRouterProvider, $stateProvider) {
 
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|tel|sms):/);
+    var isConnected = ['OAuth', '$state', '$window', 'BufferApi', function isConnected(OAuth, $state, $window, BufferApi) {
+      if(OAuth.isConnected()) {
+        return BufferApi.getProfileById(null, true).then(function success(profile) {
+          $window.location.href = $state.href('profile', { profileId: profile._id });
+        });
+      } else {
+        return false;
+      }
+    }];
 
-    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginCtrl',
+        resolve: {
+          isConnected: isConnected
+        }
+      })
+      .state('start', {
+        url: '/start',
+        templateUrl: 'app/start/start.html',
+        controller: 'StartCtrl',
+        resolve: {
+          isConnected: isConnected
+        }
+      })
+      .state('profile', {
+        url: '/profile/:profileId',
+        templateUrl: 'app/profile/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          profiles: ['BufferApi', function profiles(BufferApi) {
+            return BufferApi.getProfiles();
+          }],
+          currentProfile: ['BufferApi', function currentProfile(BufferApi) {
+            return BufferApi.getCurrentProfile();
+          }]
+        }
+      });
+
+    $urlRouterProvider.otherwise('/login');
   }]);
